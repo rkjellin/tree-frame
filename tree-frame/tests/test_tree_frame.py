59171b7927158b4c3325f4b668161e8c2b716f65
Rickard Kjellin <rickard.kjellin@gmail.com>
2022-06-12 14:24:56 +0200

    configure typechecking

--- tree-frame/tests/test_tree_frame.py
+++ tree-frame/tests/test_tree_frame.py
@@ -1,7 +1,7 @@
 from tree_frame.tree import TreeFrame
 
 
-def test_from_records():
+def test_from_records() -> None:
     x = TreeFrame.from_records(
         [{"x": 5, "y": 4, "children": [{"x": 3, "y": 6}]}], "children"
     )
